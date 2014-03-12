661fa58c60d6277f3cadaea545e60fc87db29dab
Martin Zagora <zaggino@gmail.com>
2014-03-13 06:51:53 +1100

    Fixed settings dialog labels

--- nls/root/strings.js
+++ nls/root/strings.js
@@ -10,7 +10,8 @@ define({
     RESTART:                            "Restart",
     Q_RESTART_BRACKETS:                 "Do you wish to restart Brackets to apply new settings?",
     SHORTCUTS:                          "Shortcuts",
-    SHORTCUTS_HINT:                     "Separate keys with - like this: Ctrl-Alt-Space",
+    SHORTCUTS_HINT:                     "Shortcuts - separate keys with dash like this: Ctrl-Alt-Space",
     SHOW_SNIPPET_PANEL:                 "Show snippets panel",
+    SNIPPETS_DIRECTORY:                 "Snippets directory",
     SNIPPETS_SETTINGS:                  "Snippets Settings"
 });
