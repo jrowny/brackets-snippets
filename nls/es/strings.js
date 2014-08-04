c54f1ec4e538efbb8ac855c5c26b229f3a20ddf9
Iván Barcia <contacto@ivanbarcia.eu>
2014-08-05 01:51:01 +0200

    Add Spanish language (es)

---
+++ nls/es/strings.js
@@ -0,0 +1,17 @@
+/*jshint maxlen:false */
+/*global define */
+
+define({
+    BUTTON_CANCEL:                      "Cancelar",
+    BUTTON_OK:                          "OK",
+    BUTTON_RESTORE_DEFAULTS:            "Restaurar",
+    BUTTON_SAVE:                        "Guardar",
+    INSERT_SNIPPET:                     "Intertar snippet",
+    RESTART:                            "Reiniciar",
+    Q_RESTART_BRACKETS:                 "¿Quieres reiniciar Brackets para aplicar los nuevos cambios?",
+    SHORTCUTS:                          "Atajos",
+    SHORTCUTS_HINT:                     "Atajos - separa las teclas con guiones: Ctrl-Alt-Space",
+    SHOW_SNIPPET_PANEL:                 "Mostrar panel de snippets",
+    SNIPPETS_DIRECTORY:                 "Directorio de los snippets",
+    SNIPPETS_SETTINGS:                  "Ajustes de Snippets"
+});
