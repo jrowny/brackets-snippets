00845d86e329fd9e8a25a0208f278939fc0a0d12
Daniel Kratz <git@danielkratz.com>
2014-10-02 23:27:50 +0200

    Better translation for "Shortcut"

--- nls/de/strings.js
+++ nls/de/strings.js
@@ -9,8 +9,8 @@ define({
     INSERT_SNIPPET:                     "Snippet einfügen",
     RESTART:                            "Neustart",
     Q_RESTART_BRACKETS:                 "Möchtest du Brackets neustarten um die Einstellungen anzuwenden?",
-    SHORTCUTS:                          "Shortcuts",
-    SHORTCUTS_HINT:                     "Shortcuts - Tasten mit Bindestrich trennen, z.B.: Ctrl-Alt-Space",
+    SHORTCUTS:                          "Tastenkürzel",
+    SHORTCUTS_HINT:                     "Tastenkürzel - Tasten mit Bindestrich trennen, z.B.: Ctrl-Alt-Space",
     SHOW_SNIPPET_PANEL:                 "Snippets Menü anzeigen",
     SNIPPETS_DIRECTORY:                 "Snippets Verzeichnis",
     SNIPPETS_SETTINGS:                  "Snippets Einstellungen"
