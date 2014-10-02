1922e44f919250c228dc87bde8c8da090c3e546e
Daniel Kratz <git@danielkratz.com>
2014-10-02 23:25:30 +0200

    Updated german translation

--- nls/de/strings.js
+++ nls/de/strings.js
@@ -3,6 +3,15 @@
 
 define({
     BUTTON_CANCEL:                      "Abbrechen",
-    BUTTON_DEFAULTS:                    "Standard wiederherstellen",
-    BUTTON_SAVE:                        "Speichern"
+    BUTTON_OK:                          "OK",
+    BUTTON_RESTORE_DEFAULTS:            "Standard wiederherstellen",
+    BUTTON_SAVE:                        "Speichern",
+    INSERT_SNIPPET:                     "Snippet einfügen",
+    RESTART:                            "Neustart",
+    Q_RESTART_BRACKETS:                 "Möchtest du Brackets neustarten um die Einstellungen anzuwenden?",
+    SHORTCUTS:                          "Shortcuts",
+    SHORTCUTS_HINT:                     "Shortcuts - Tasten mit Bindestrich trennen, z.B.: Ctrl-Alt-Space",
+    SHOW_SNIPPET_PANEL:                 "Snippets Menü anzeigen",
+    SNIPPETS_DIRECTORY:                 "Snippets Verzeichnis",
+    SNIPPETS_SETTINGS:                  "Snippets Einstellungen"
 });
