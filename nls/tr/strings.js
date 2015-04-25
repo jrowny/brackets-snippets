541ccebbc285f5cbb2ec7ae9c68fa84b7ac41d92
Nazım Can <canaltinova@gmail.com>
2015-04-25 18:05:15 +0300

    Added Turkish Language

---
+++ nls/tr/strings.js
@@ -0,0 +1,17 @@
+/*jshint maxlen:false */
+/*global define */
+
+define({
+    BUTTON_CANCEL:                      "İptal",
+    BUTTON_OK:                          "Tamam",
+    BUTTON_RESTORE_DEFAULTS:            "Varsayılanı geri yükle",
+    BUTTON_SAVE:                        "Kaydet",
+    INSERT_SNIPPET:                     "Snippet ekle",
+    RESTART:                            "Yeniden başlat",
+    Q_RESTART_BRACKETS:                 "Yeni ayarları uygulamak için Brackets'ı yeniden başlatmak istiyor musunuz?",
+    SHORTCUTS:                          "Kısayollar",
+    SHORTCUTS_HINT:                     "Kısayollar - tuşları buradaki gibi çizgi ile ayırınız: Ctrl-Alt-Space",
+    SHOW_SNIPPET_PANEL:                 "Snippets panelini göster",
+    SNIPPETS_DIRECTORY:                 "Snippets dizini",
+    SNIPPETS_SETTINGS:                  "Snippets Ayarları"
+});
